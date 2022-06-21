aea91f89e874438f80e604cd5113455a74ae1d20
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2022-06-21 21:27:36 +0200

    fm175xx: fix reading/writing LPCD regs over i2c

--- rnfc-fm175xx/src/interface/i2c.rs
+++ rnfc-fm175xx/src/interface/i2c.rs
@@ -11,14 +11,30 @@ impl<T: I2c> I2cInterface<T> {
     pub fn new(i2c: T, address: u8) -> Self {
         Self { i2c, address }
     }
+
+    fn read_reg_raw(&mut self, reg: u8) -> u8 {
+        let mut buf = [0; 1];
+        self.i2c.write_read(self.address, &[reg], &mut buf).unwrap();
+        buf[0]
+    }
+
+    fn write_reg_raw(&mut self, reg: u8, val: u8) {
+        self.i2c.write(self.address, &[reg as u8, val]).unwrap();
+    }
 }
 
 impl<T: I2c> Interface for I2cInterface<T> {
     fn read_reg(&mut self, reg: usize) -> u8 {
         let reg = reg as u8;
-        let mut buf = [0; 1];
-        self.i2c.write_read(self.address, &[reg], &mut buf).unwrap();
-        let res = buf[0];
+        let res = if reg < 0x40 {
+            // Main register
+            self.read_reg_raw(reg)
+        } else {
+            // Extended register
+            let reg = reg - 0x40;
+            self.write_reg_raw(0x0f, reg | 0x80);
+            self.read_reg_raw(0x0f) & 0x3F
+        };
         trace!("     read {=u8:02x} = {=u8:02x}", reg, res);
         res
     }
@@ -26,7 +42,16 @@ impl<T: I2c> Interface for I2cInterface<T> {
     fn write_reg(&mut self, reg: usize, val: u8) {
         let reg = reg as u8;
         trace!("     write {=u8:02x} = {=u8:02x}", reg, val);
-        self.i2c.write(self.address, &[reg as u8, val]).unwrap();
+
+        if reg < 0x40 {
+            // Main register
+            self.write_reg_raw(reg, val)
+        } else {
+            // Extended register
+            let reg = reg - 0x40;
+            self.write_reg_raw(0x0F, reg | 0x40);
+            self.write_reg_raw(0x0F, (val & 0x3F) | 0xC0);
+        }
     }
 
     fn read_fifo(&mut self, data: &mut [u8]) {
