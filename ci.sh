4a88e124ab1cc8c86293245a79af72610b52f00b
Dario Nieuwenhuis <dirbaio@dirbaio.net>
2022-06-28 03:02:40 +0200

    ci script fix

--- ci.sh
+++ ci.sh
@@ -6,8 +6,8 @@ export CARGO_TARGET_DIR=$PWD/target_ci
 export RUSTFLAGS=-Dwarnings
 export DEFMT_LOG=trace
 
-find . -name '*.rs' -not -path '*target*' -not -path '*stm32-metapac-gen/out/*' -not -path '*stm32-metapac/src/*' | xargs rustfmt --check  --skip-children --unstable-features --edition 2018
+find . -name '*.rs' -not -path '*target*' | xargs rustfmt --check  --skip-children --unstable-features --edition 2018
 
 cargo batch  \
     --- build --release --manifest-path examples/st25r39-disco/Cargo.toml --target thumbv7em-none-eabi \
-#    --- build --release --manifest-path examples/fm175xx/Cargo.toml --target thumbv7em-none-eabi \
+    --- build --release --manifest-path examples/fm175xx/Cargo.toml --target thumbv7em-none-eabi \
